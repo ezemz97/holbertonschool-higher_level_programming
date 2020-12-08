e610a0a621ee7ca4f45746abd55bdddde9ca926c
ezedksl <ezedksl@gmail.com>
2020-12-08 21:17:44 +0000

    upd

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
+            letter = chr(ord(letter) - 32)
+            print(letter, end="")
+        else:
+            print(letter, end="")
+    print()
