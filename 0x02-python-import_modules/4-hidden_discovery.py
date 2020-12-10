76db8ed963acf681ac8322c25764d6fac33add7b
ezedksl <ezedksl@gmail.com>
2020-12-10 18:07:32 +0000

    upd

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
     for names in dir(hidden_4):
         if names[1] != '_':
             print("{}".format(names))
