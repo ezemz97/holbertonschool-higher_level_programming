cee9a155a5b95c1750796580220fd0beacada795
Eze <ezedksl@gmail.com>
2020-12-10 23:26:07 +0000

    fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv
 if __name__ == "__main__":
-    xargv = len(argv) - 1
-    if xargv != 3:
-        print ("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    if argv[2] == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif argv[2] == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif argv[2] == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif argv[2] == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
-    else:
+    a, b = int(argv[1]), int(argv[3])
+    operator = argv[2]
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, ops[operator](a, b)))
