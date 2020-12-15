cd5bae9cc0312effc05d476e7352f79170448f64
ezedksl <ezedksl@gmail.com>
2020-12-15 19:30:11 +0000

    added 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in range (len(matrix)):
+        for y in range (len(matrix[x])):
+            if y + 1 == len(matrix[x]):
+                print("{:d}".format(matrix[x][y]), end="")
+            else:
+                print("{:d}".format(matrix[x][y]), end=" ")
+        print("")
