3a6205cb04cadeb1f9d454825cee378208fd737e
ezedksl <ezedksl@gmail.com>
2020-12-15 21:24:22 +0000

    added 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    find = 0
+    if my_list
+        for x in my_list:
+            if find < x:
+                find = x
+        return find
+    else:
+        return None
+
+        
