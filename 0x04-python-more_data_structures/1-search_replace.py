73decfcb94d9d55f7afa96824ee37fea1aa0c48c
ezedksl <ezedksl@gmail.com>
2020-12-17 18:55:16 +0000

    update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if (my_list):
-        newlist = my_list.copy()
-        newlist = [replace if value == search else value for value in newlist]
-        return newlist
-    return None
+    newlist = my_list.copy()
+    newlist = [replace if value == search else value for value in newlist]
+    return newlist
