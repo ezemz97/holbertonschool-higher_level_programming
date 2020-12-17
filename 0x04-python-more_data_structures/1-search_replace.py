8736c92fff5832d08a95974812a259fd8cbfbe57
ezedksl <ezedksl@gmail.com>
2020-12-17 18:44:11 +0000

    update

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+def search_replace(my_list, search, replace):
+    if (my_list):
+        newlist = my_list.copy()
+        newlist = [replace if value == search else value for value in newlist]
+        return newlist
+    return None
