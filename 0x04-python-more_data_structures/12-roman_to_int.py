5f36638c4e0a89c6010a9e1de9a37b7a33e27e19
ezedksl <ezedksl@gmail.com>
2020-12-18 19:31:00 +0000

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-
-roman_string = "LXXXVII"
-romans = {"M":1000, "D":500, "C":100, "L":50, "X": 10, "V":5, "I":1}
-new = []
-total = 0
-for w in roman_string:
-    new.append(romans[w])
-print (new)
-prev = 0
-z = -1
-for x in range(len(new)):
-    if new[x] >= prev:
-        total += prev
-    else:
-        total -= prev
-    prev = new[z+1]
+def roman_to_int(roman_string):
+    romans = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    new = []
+    total = 0
+    for w in roman_string:
+        new.append(romans[w])
+    y = 0
+    lenmenosuno = len(new) - 1
+    for x in range(len(new)):
+        if (y < lenmenosuno):
+            y = x + 1
+            if new[x] >= new[y]:
+                total += new[x]
+            else:
+                total -= new[x]
+    total += new[lenmenosuno]
+    return total
     
-
-print (total)
