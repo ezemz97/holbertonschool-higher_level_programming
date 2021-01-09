857357cf89aa3f882e02672da3a75deaa6c97366
Eze <ezedksl@gmail.com>
2021-01-09 20:37:20 +0000

    update

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+"""A single function module,
+the function adds two numbers
+
+"""
+def add_integer(a, b=98):
+    """This function sums two numbers
+
+    Args:
+        a (int)(float): First number to add
+        b (int)(float): Second number to add
+    
+    Returns:
+        The product of a + b
+    
+    Raises:
+        TypeError if arguments not (int) or (float)
+    
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
