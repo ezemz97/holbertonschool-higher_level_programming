d7e304822c50ebba0505dfdd3c3f5f8dfea1c987
Ezequiel <ezedksl@gmail.com>
2021-01-10 11:39:07 +0000

    udp

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """A single function module,
 the function adds two numbers
 
@@ -16,7 +17,10 @@ def add_integer(a, b=98):
         TypeError if arguments not (int) or (float)
     
     """
-
+    if isinstance(a, bool):
+        a = "hello darkness"
+    if isinstance(b, bool):
+        b = "my old friend"
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
