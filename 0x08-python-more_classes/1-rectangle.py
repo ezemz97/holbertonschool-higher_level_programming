6d36e1f5bb19ad4864c5ab8b240420ee6255dc5d
Ezequiel <ezedksl@gmail.com>
2021-01-11 22:21:31 +0000

    que task de mierda el 4 bo vamoarriba

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,15 @@ class Rectangle:
 
         """
 
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
 
     #width methods
