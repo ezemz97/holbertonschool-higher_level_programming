3281e92f87af96c094dae5f4d69ada254b014b6d
Eze <ezedksl@gmail.com>
2021-01-13 01:40:36 +0000

    update

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,20 +1,3 @@
-"""Function to copy a list"""
-
-
+#!/usr/bin/python3
 def copy_list(l):
-    """Copy a list and return the new list
-
-    Args:
-        l (list): List to copy
-
-    Returns:
-        The new list
-
-    Raises:
-        TypeError: When the list is not list, or empty
-    """
-    if type(l) is not list:
-        raise TypeError("Argument must be a list.")
-    new_list = []
-    new_list = l[:]
-    return new_list
+    return l.copy()
