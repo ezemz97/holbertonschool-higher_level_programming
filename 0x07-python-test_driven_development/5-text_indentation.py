1c4c4b89501b85ffd2232ab7c43397ffe6c68dc8
Eze <ezedksl@gmail.com>
2021-01-14 00:45:16 +0000

    update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -21,18 +21,45 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
+    white = False
+    for char in text:
+        if char is " ":
+            if white is True:
+                continue
+            else:
+                print("{}".format(char), end="")
+                white = True
+        elif char is ":" or char is "." or char is "?":
+            print("{}".format(char), end="\n")
+            white = True
+        else:
+            print("{}".format(char), end="")
+            white = False
+
+
+
+
+
+
+
+
+
+
+
+    '''
     whitespace = False
-    for character in text:
+    for char in text:
         if (
-            character is not "." and character is not ":"
-            and character is not "?"
+            char is not "." and char is not ":"
+            and char is not "?"
         ):
             if whitespace is False:
-                print("{}".format(character), end="")
-            elif whitespace is True and character is " ":
+                print("{}".format(char), end="")
+            elif whitespace is True and char is " ":
                 whitespace = False
                 continue
         else:
-            print("{}".format(character))
+            print("{}".format(char))
             print()
             whitespace = True
+    '''
