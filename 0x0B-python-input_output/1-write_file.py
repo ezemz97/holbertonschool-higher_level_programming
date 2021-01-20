92230c5c22a2420adad03af05834f33535f149b8
ezedksl <ezedksl@gmail.com>
2021-01-20 21:28:52 +0000

    added task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+"""Single function to write text to a file
+
+Example: write_file("textfile.txt", "hello world")
+
+"""
+
+
+def write_file(filename="", text=""):
+    """Write text to a file (UTF-8)
+
+    Args:
+        filename (str): Name of the file to open
+        text (str): Text to write to the file
+    
+    Returns:
+        Number of characters written
+
+    """
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return file.write(text)
