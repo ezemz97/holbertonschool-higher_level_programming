a5216fe219b84542db8e2c00682662ae619ad5f5
Eze <ezedksl@gmail.com>
2021-01-20 23:18:57 +0000

    update

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Encodes a python object into a json"""
-
-
 import json
+
+
 def to_json_string(my_obj):
-    """Returns the serialization of an object"""
+    """Returns the json serialization of an object"""
     return json.dumps(my_obj)
