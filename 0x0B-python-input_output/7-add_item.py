470795ff2b507fd14e791da2290860f704011d9a
Eze <ezedksl@gmail.com>
2021-01-21 22:25:43 +0000

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""This is a single function module."""
-import json
+"""Add item"""
 from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-# Save arguments to new list.
-args_list = argv[1:]
-
-# Variable with the name of the json file to modify.
 filename = "add_item.json"
-
-# Try to open json file, and create new list with
-# the contents of the file and the arguments list.
-# If the file does not exist, it should be created,
-# and arguments will be written.
 try:
-    jfile = load_from_json_file(filename)
-    jlist = list(jfile)
-    final_list = jlist + args_list
-    save_to_json_file(final_list, filename)
-except FileNotFoundError:
-    save_to_json_file(args_list, filename)
+    my_list = load_from_json_file(filename)
+except:
+    my_list = []
+for arg in argv[1:]:
+    my_list.append(arg)
+save_to_json_file(my_list, filename)
