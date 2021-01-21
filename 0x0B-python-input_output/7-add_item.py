39c910ccbb7f8aaba60f1fe7add37a3ef2200027
Eze <ezedksl@gmail.com>
2021-01-21 22:21:06 +0000

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list, and then save them to a file"""
-import sys
+"""This is a single function module."""
 import json
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-load_json = __import__('8-load_from_json_file').load_from_json_file
-save_json = __import__('7-save_to_json_file').save_to_json_file
+# Save arguments to new list.
+args_list = argv[1:]
 
+# Variable with the name of the json file to modify.
+filename = "add_item.json"
 
+# Try to open json file, and create new list with
+# the contents of the file and the arguments list.
+# If the file does not exist, it should be created,
+# and arguments will be written.
 try:
-    my_list = load_json("add_item.json")
+    jfile = load_from_json_file(filename)
+    jlist = list(jfile)
+    final_list = jlist + args_list
+    save_to_json_file(final_list, filename)
 except FileNotFoundError:
-    my_list = []
-
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-
-save_json(my_list, filename)
+    save_to_json_file(args_list, filename)
