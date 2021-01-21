fe013331a364ed13fffcf6ebed5df971db8aa9f3
Eze <ezedksl@gmail.com>
2021-01-21 01:32:03 +0000

    added task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,1 @@
-#!/usr/bin/python3
-"""Add item"""
-from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-
-filename = "add_item.json"
-try:
-    my_list = load_from_json_file(filename)
-except:
-    my_list = []
-for arg in argv[1:]:
-    my_list.append(arg)
-save_to_json_file(my_list, filename)
+fk this
