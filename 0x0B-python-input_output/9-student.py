b53ce296f718ab60637a55c0858d73b19bf94452
Eze <ezedksl@gmail.com>
2021-01-21 23:54:56 +0000

    finish upload

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Student class"""
+""" Student """
 
 
-class Students:
-    """Student class"""
+class Student():
+    """ Class student """
     def __init__(self, first_name, last_name, age):
-        """Initializes a student"""
+        """ Initializes a student """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Returns the dictionary representation
-        of a student"""
+        """ function """
         return self.__dict__
