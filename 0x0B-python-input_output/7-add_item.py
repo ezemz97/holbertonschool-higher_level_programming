5afd592e8777fc7172776ad46d2a1300b542f0f5
Eze <ezedksl@gmail.com>
2021-01-21 01:29:35 +0000

    added task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines a function to save a list of arguments to a file"""
 import json
-import sys
+from sys import argv
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
@@ -12,5 +12,4 @@ except FileNotFoundError:
     plist = []
 for arg in sys.argv[1:]:
     plist.append(arg)
-
 save(plist, "add_item.json")
