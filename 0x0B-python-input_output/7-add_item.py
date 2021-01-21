0211b755b41ff0742a49d4f5c4c556af77defee8
Eze <ezedksl@gmail.com>
2021-01-21 22:20:12 +0000

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 """adds all arguments to a Python list, and then save them to a file"""
 import sys
 import json
+
 
 load_json = __import__('8-load_from_json_file').load_from_json_file
 save_json = __import__('7-save_to_json_file').save_to_json_file
