fc2e56efa5e51b6143a67a7cea1c298563417112
Eze <ezedksl@gmail.com>
2021-01-21 23:55:57 +0000

    finish upload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Description"""
+""" Student """
 
 
-class Student:
+class Student():
+    """ Class student """
     def __init__(self, first_name, last_name, age):
-        """Initializes a student"""
+        """ Initializes a student """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
