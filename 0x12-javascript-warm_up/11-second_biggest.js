b7d65cd4fa0ff1024e29552c0bad2c44cbf5b089
S <ezedksl@gmail.com>
2021-03-22 22:04:17 -0300

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
-const argList = process.argv;
-if (argList.length <= 3) {
-  console.log(0);
-} else {
-  console.log(argList.sort().reverse()[1]);
-}
-/* My code that works but the checker doesn't like
-
 let i = 0;
 let biggest = parseInt(process.argv[2]); let second = parseInt(process.argv[2]);
 
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  for (i = 3; i < process.argv.length; i++) {
+  for (i = 3; i < process.argv.length - 2; i++) {
     if (parseInt(process.argv[i]) > biggest) {
       second = biggest;
       biggest = parseInt(process.argv[i]);
@@ -21,4 +13,3 @@ if (process.argv.length <= 3) {
   }
   console.log(second);
 }
-*/
