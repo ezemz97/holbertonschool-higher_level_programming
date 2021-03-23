349a772dd42d6e4ee58474c0b3e7f01756a7b3b3
S <ezedksl@gmail.com>
2021-03-22 22:05:03 -0300

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,21 @@
 #!/usr/bin/node
+const argLen = (process.argv.length) - 2;
+if (argLen === 0 || argLen === 1) {
+  console.log(0);
+} else {
+  const prev = process.argv;
+  prev.sort(function (a, b) { return a - b; });
+  console.log(prev[prev.length - 2]);
+}
+
+/* My code
 let i = 0;
 let biggest = parseInt(process.argv[2]); let second = parseInt(process.argv[2]);
 
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  for (i = 3; i < process.argv.length - 2; i++) {
+  for (i = 3; i < process.argv.length; i++) {
     if (parseInt(process.argv[i]) > biggest) {
       second = biggest;
       biggest = parseInt(process.argv[i]);
@@ -13,3 +23,4 @@ if (process.argv.length <= 3) {
   }
   console.log(second);
 }
+*/
