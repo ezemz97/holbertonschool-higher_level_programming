a87cb735bbda2dac59c1056369137998bcd53c5d
Eze <ezedksl@gmail.com>
2021-08-07 21:39:43 -0300

    task 9 please no errors

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" List all State objects containing "a" from db (ORM)
+
+Here we use Session as interface to the database.
+
+Args:
+    user (str): mysql username
+    pass (str): mysql password
+    database (str): mysql database name
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id)\
+                        .filter("a" in str(State.name)):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
