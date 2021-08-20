80936362a54e666ca5f7ee414e73666a8907331f
Eze <ezedksl@gmail.com>
2021-08-20 19:43:28 -0300

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,7 +14,7 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.get(argv[1])
-    if r.raise_for_status:
+    if r.raise_for_status():
         print("Error code: {}".format(r.status_code))
     else:
         print("{}".format(r.text))
