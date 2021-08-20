1f4b2d1cba3fc9bd7a9f7612ee6285b6a4f56d2e
Eze <ezedksl@gmail.com>
2021-08-20 19:51:07 -0300

    fix 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Sends a POST request to the passed URL with the email as a parameter.
+"""Sends a request to the URL and displays the body of the response.
 
-Displays the body of the response.
+Prints the error code (if)
 
 Args:
     url: Destination URL
-    email: Email value
 """
 
 
@@ -14,7 +13,9 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.get(argv[1])
-    if r.raise_for_status():
+    try:
+        r.raise_for_status()
+    except requests.exceptions.HTTPError:
         print("Error code: {}".format(r.status_code))
     else:
         print("{}".format(r.text))
