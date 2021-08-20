daeed74a8b7d4422329cb66e5827af6c16656813
Eze <ezedksl@gmail.com>
2021-08-20 19:19:23 -0300

    add task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Sends a POST request to the passed URL with the email as a parameter.
+
+Displays the body of the response.
+
+Args:
+    url: Destination URL
+    email: Email value
+"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.post(argv[1], data = {'email':argv[2]})
+    print(r.text)
