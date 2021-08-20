845499cfd10c74f07c5a6102c881ec9bc18ce7a3
Eze <ezedksl@gmail.com>
2021-08-19 23:20:09 -0300

    upd

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,13 +10,14 @@ def find_peak(list_of_integers):
     Doesn't take into account the first and last elements as peak
     """
 
-    mid_idx = len(list_of_integers) // 2
-
-    if len(list_of_integers) == 0 or list_of_integers is None:
+    if list_of_integers == []:
         return None
-    elif len(list_of_integers) < 3:
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+    elif len(list_of_integers) == 2:
         return max(list_of_integers)
     else:
+        mid_idx = len(list_of_integers) // 2
         if (list_of_integers[mid_idx] >= list_of_integers[mid_idx + 1] and
                 list_of_integers[mid_idx] >= list_of_integers[mid_idx - 1]):
             return list_of_integers[mid_idx]
