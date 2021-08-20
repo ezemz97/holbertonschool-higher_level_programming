2a9d2225cb296c49275afb2cb74fb15635e09ea7
Eze <ezedksl@gmail.com>
2021-08-20 16:59:36 -0300

    initial commit - first task added - output OK

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status
+
+Prints the type of the response,
+the content, and the utf-8 decoded content.
+
+"""
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    decode = html.decode('utf-8')
+    print("Body response:\n "
+          "\t- type: {}\n "
+          "\t- content: {}\n "
+          "\t- utf8 content: {}"
+          .format(type(html), html, decode))
