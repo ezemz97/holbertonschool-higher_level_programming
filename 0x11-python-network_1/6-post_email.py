c6c8e7101a2ac7b5274bf648445e78c9df7c8c1c
Eze <ezedksl@gmail.com>
2021-08-20 19:20:49 -0300

    task 6 pp8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -13,5 +13,5 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = requests.post(argv[1], data = {'email':argv[2]})
+    r = requests.post(argv[1], data={'email': argv[2]})
     print(r.text)
