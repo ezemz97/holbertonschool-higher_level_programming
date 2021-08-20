a23ec4aaa7d2db497537bedf115c68da2fd14dee
Eze <ezedksl@gmail.com>
2021-08-20 00:39:54 -0300

    first advanced

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# https://everything.curl.dev/usingcurl/verbose/writeout
 # Write out response code
+# https://everything.curl.dev/usingcurl/verbose/writeout
 
 curl -s -w '%{http_code}' $1 -o /dev/null
