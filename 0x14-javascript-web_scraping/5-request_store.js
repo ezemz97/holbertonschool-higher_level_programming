053f84111669ede562253b36efe886b858063b55
Eze <ezedksl@gmail.com>
2021-09-15 14:32:05 +0000

    update comments

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 // Requests the contents of a webpage and stores it in a UTF-8 encoded file.
 // @param {string} url: Webpage to request.
 // @param {string} path: File's full path to save.
-const url = process.argv[2]
-const path = process.argv[3]
+const url = process.argv[2];
+const path = process.argv[3];
 
 request(url, function (_err, _res, body) {
   fs.writeFile(path, body, 'utf8', function (err) {
